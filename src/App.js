91bf1464ef8ae60e5a4f92407d14de177b09c378
Beny <b.ntambwe97@gmail.com>
2021-03-18 15:35:59 +0100

    création navbar

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import AboutMe from './components/pages/AboutMe';
+import Skills from './components/pages/Skills';
+import Gallery from './components/pages/Gallery';
+import Contact from './components/pages/Contact';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+
+function App() {
+    return (
+        <>
+        <Router>
+            <Navbar/>
+        <Switch>
+            <Route path='/' exact />
+            <Route path='/skills' component={Skills}/>
+            <Route path='/gallery' component={Gallery}/>
+            <Route path='/contact' component={Contact}/>
+        </Switch>    
+        </Router>
+        </>
+    )
+}
+
+export default App;
